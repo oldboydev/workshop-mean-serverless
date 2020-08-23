8277ad1ca63e949dd8fdb26d8801040f45b5e01f
oldboydev <pm@oldboy.dev>
2020-08-23 14:28:25 -0300

    Fix - Mongo conection

--- api-serveless/shared/mongo.ts
+++ api-serveless/shared/mongo.ts
@@ -1,20 +1,17 @@
 import { MongoClient, Db } from "mongodb";
 
 export class MongoConnection{
-    private config: {
+    private config = {
         url: "mongodb://localhost:27017/crud-serveless-mongo-wavy",
         dbName: "crud-serveless-mongo-wavy"
     };
 
-    private connInfo: {
-        connection: MongoClient,
-        db: Db
-    };
-
     async createConnection(){
-        this.connInfo.connection = await MongoClient.connect(this.config.url, { useNewUrlParser: true });
-        this.connInfo.db = this.connInfo.connection.db(this.config.dbName);
+        const connection = await MongoClient.connect(this.config.url, { useNewUrlParser: true });
+        const db = connection.db(this.config.dbName);
 
-        return this.connInfo;
+        const connInfo = {connection, db};
+
+        return connInfo;
     }
 }
